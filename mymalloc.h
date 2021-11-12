1a73dbe92e29ac8d7b1ce81dd5bf3e40d95d2e47
Alay Shah <47639000+alayshahh@users.noreply.github.com>
2021-11-11 20:54:39 -0500

    header file for mymalloc

---
+++ mymalloc.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+
+void myinit(int allocAlg);
+void* mymalloc(size_t size);
+void myfree(void* ptr);
+void* myrealloc(void* ptr, size_t size);
+void mycleanup();
+double utilization();
